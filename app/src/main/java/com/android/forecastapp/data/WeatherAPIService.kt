7683898813e77663250f2225f2842b731e19c542
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2020-01-08 00:14:17 +0200

    Fetch data from the API

---
+++ app/src/main/java/com/android/forecastapp/data/WeatherAPIService.kt
@@ -0,0 +1,55 @@
+package com.android.forecastapp.data
+
+import com.android.forecastapp.data.response.CurrentWeatherResponse
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY = "5d3b356a3a783013d553997597a65dfb"
+const val BASE_URL = "http://api.weatherstack.com/"
+
+interface WeatherAPIService {
+
+    @GET("current")
+    fun getCurrentWeather(
+        @Query("query") location: String
+    ): Deferred<CurrentWeatherResponse>
+
+    companion object {
+        operator fun invoke(): WeatherAPIService {
+            val requestInterceptor = Interceptor { chain ->
+
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("access_key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+
+                return@Interceptor chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WeatherAPIService::class.java)
+        }
+    }
+
+}
