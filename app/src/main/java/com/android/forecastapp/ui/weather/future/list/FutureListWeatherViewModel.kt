618badd1bbfc8db719529b4bb5560491c3b77394
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2020-01-07 19:08:28 +0200

    Navigation & App Foundation

---
+++ app/src/main/java/com/android/forecastapp/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.forecastapp.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
