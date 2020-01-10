214a3062e11d225d427d03f0206846d963ae62ea
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2020-01-11 01:59:38 +0200

    Current Weather User Interface

--- app/src/main/java/com/android/forecastapp/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/android/forecastapp/ui/weather/current/CurrentWeatherFragment.kt
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 
 import com.android.forecastapp.R
 import com.android.forecastapp.data.network.ConnectivityInterceptorImpl
 import com.android.forecastapp.data.network.WeatherAPIService
 import com.android.forecastapp.data.network.WeatherNetworkDataSourceImpl
+import com.android.forecastapp.internal.glide.GlideApp
 import com.android.forecastapp.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.coroutines.Dispatchers
@@ -51,8 +53,48 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
         currentWeather.observe(this@CurrentWeatherFragment, Observer {
             if (it == null) return@Observer
 
-            textView.text = it.toString()
+            group_loading.visibility = View.GONE
+            updateLocation("London")
+            updateDateToToday()
+            updateTemperature(it.temperature,it.feelsLikeTemperature)
+            updateCondition(it.conditionText)
+            updatePrecipitation(it.precipitationVolume)
+            updateWind(it.windDirection, it.windSpeed)
+            updateVisibility(it.visibilityDistance)
+
+            GlideApp.with(this@CurrentWeatherFragment)
+                .load(it.conditionIconUrl[0])
+                .into(imageView_condition_icon)
         })
+    }
+
+    private fun updateLocation(location: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToToday() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Today"
+    }
+
+    private fun updateTemperature(temperature: Double, feelsLike: Double){
+        textView_temperature.text = "$temperature°C"
+        textView_feels_like_temperature.text = "Feels like $feelsLike°C"
+    }
+
+    private fun updateCondition(condition: List<String>) {
+        textView_condition.text = condition[0]
+    }
+
+    private fun updatePrecipitation(precipitationVolume: Double) {
+        textView_precipitation.text = "Preciptiation: $precipitationVolume mm"
+    }
+
+    private fun updateWind(windDirection: String, windSpeed: Double) {
+        textView_wind.text = "Wind: $windDirection, $windSpeed kph"
+    }
+
+    private fun updateVisibility(visibilityDistance: Double) {
+        textView_visibility.text = "Visibility: $visibilityDistance km"
     }
 
 }
